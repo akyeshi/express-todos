27f82ab7c27a5fa0cd008b9698bdfd0a59bd415a
akyeshi <akyeshi@outlook.com>
2024-04-10 13:07:42 -0400

    changes made

--- controllers/todos.js
+++ controllers/todos.js
@@ -3,17 +3,17 @@ const Todo = require("../models/todo");
 module.exports = {
   // first index is the name of the property on the todosCtrl(controller)
   // second index is the name of the function below
-  index: index, 
-  show
+  index: index,
+  show,
 };
 
-
 function show(req, res) {
-  // res.send(req.params); 
-  // res.send(req.body); 
-  res.render('todos/show', {
-    todo: Todo.getOne(req.params.id)
-  })
+  // res.send(req.params);
+  // res.send(req.body);
+  res.render("todos/show", {
+    todo: Todo.getOne(req.params.id),
+    title: `To-Do Detail (#${req.params.id})`,
+  });
 }
 
 // controllers/todos.js
@@ -23,5 +23,6 @@ function index(req, res) {
   // render view, also pass in data
   res.render("todos/index", {
     todos: Todo.getAll(),
+    title: "All To-Dos",
   });
 }
